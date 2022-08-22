e5e74bdd0fcfc5c5943302e8ec21a2f80a15b2a6
Arnaldo Pereira Silva <arnaldopereirasilva99@gmail.com>
2022-08-22 16:12:37 -0300

    add and config celebrate and fixes.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './shared/http/routes';
 import AppError from '@shared/errors/AppErrors';
 import '@shared/typeorm';
@@ -10,9 +11,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
+app.use(errors());
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
@@ -21,8 +21,6 @@ app.use(
         message: error.message,
       });
     }
-
-    console.log(error);
 
     return response.status(500).json({
       status: 'error',
