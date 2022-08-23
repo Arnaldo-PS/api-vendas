83fd962df4e085d9fcd6e758945eb7b5dfdd18da
Arnaldo Pereira Silva <arnaldopereirasilva99@gmail.com>
2022-08-23 13:56:27 -0300

    lib multer config

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,7 +3,7 @@ import { verify } from 'jsonwebtoken';
 import AppError from '@shared/errors/AppErrors';
 import authconfig from '@config/auth';
 
-interface TokenPayload {
+interface ITokenPayload {
   iat: number;
   exp: number;
   sub: string;
@@ -25,7 +25,7 @@ export default function isAuthenticated(
   try {
     const decodedToken = verify(token, authconfig.jwt.secret);
 
-    const { sub } = decodedToken as TokenPayload;
+    const { sub } = decodedToken as ITokenPayload;
 
     request.user = {
       id: sub,
