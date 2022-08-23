fbb50f53716a96c17df9ef84ac16b6b0cbbde64a
Arnaldo Pereira Silva <arnaldopereirasilva99@gmail.com>
2022-08-23 10:13:56 -0300

    user service fixes.

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import DeleteUserService from '../services/DeleteUserService';
+import ListUserService from '../services/ListUserService';
+import ShowUserService from '../services/ShowUserService';
+
+export default class UsersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUser = new ListUserService();
+
+    const users = await listUser.execute();
+
+    return response.json(users);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteUser = new DeleteUserService();
+
+    await deleteUser.execute({ id });
+
+    return response.json([]);
+  }
+
+  public async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const showUser = new ShowUserService();
+
+    const user = await showUser.execute({ id });
+
+    return response.json(user);
+  }
+}
