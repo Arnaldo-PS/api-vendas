52bd358b5a64aa3392917ca8eba1b7fb54a2d281
Arnaldo Pereira Silva <arnaldopereirasilva99@gmail.com>
2022-08-24 13:30:20 -0300

    add and config lib nodemailer.

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppErrors';
 import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -19,6 +20,11 @@ class SendForgotPasswordEmailService {
     }
 
     const token = await userTokensRepository.generate(user.id);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+    });
   }
 }
 
