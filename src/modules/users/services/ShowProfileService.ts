a87147dad87faeb5b98484008c188df57018bd9a
Arnaldo Pereira Silva <arnaldopereirasilva99@gmail.com>
2022-08-25 15:56:53 -0300

    add profile controller and fixes.

--- src/modules/users/services/ShowProfileService.ts
+++ src/modules/users/services/ShowProfileService.ts
@@ -1,25 +1,14 @@
 import AppError from '@shared/errors/AppErrors';
-import { compare, hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 
 interface IRequest {
   user_id: string;
-  name: string;
-  email: string;
-  password?: string;
-  old_password?: string;
 }
 
-export default class ShowProfileService {
-  public async execute({
-    user_id,
-    name,
-    email,
-    password,
-    old_password,
-  }: IRequest): Promise<User> {
+class ShowProfileService {
+  public async execute({ user_id }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
 
     const user = await usersRepository.findById(user_id);
@@ -28,31 +17,8 @@ export default class ShowProfileService {
       throw new AppError('User not found.');
     }
 
-    const userUpdateEmail = await usersRepository.findByEmail(email);
-
-    if (userUpdateEmail && userUpdateEmail.id !== user_id) {
-      throw new AppError('There is already one user with this email.');
-    }
-
-    if (password && !old_password) {
-      throw new AppError('Old password is required.');
-    }
-
-    if (password && old_password) {
-      const checkOldPassword = await compare(old_password, user.password);
-
-      if (!checkOldPassword) {
-        throw new AppError('Old password does not match.');
-      }
-
-      user.password = await hash(password, 8);
-    }
-
-    user.name = name;
-    user.email = email;
-
-    await usersRepository.save(user);
-
     return user;
   }
 }
+
+export default ShowProfileService;
