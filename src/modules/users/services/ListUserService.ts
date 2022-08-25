61cea86023ded540197d4712e5a6a07e728d5780
Arnaldo Pereira Silva <arnaldopereirasilva99@gmail.com>
2022-08-25 15:03:15 -0300

    add profile services.

--- src/modules/users/services/ListUserService.ts
+++ src/modules/users/services/ListUserService.ts
@@ -2,7 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 
-class ListUserService {
+export default class ListUserService {
   public async execute(): Promise<User[]> {
     const usersRepository = getCustomRepository(UsersRepository);
 
@@ -11,5 +11,3 @@ class ListUserService {
     return users;
   }
 }
-
-export default ListUserService;
---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,22 @@
+import AppError from '@shared/errors/AppErrors';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+export default class UpdateProfileService {
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    return user;
+  }
+}
