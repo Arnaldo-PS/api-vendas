0d27864d5bb7543da2e8536586da438bc038d09f
Arnaldo <arnaldopereirasilva99@gmail.com>
2022-09-08 10:32:58 -0300

    add typeorm pagination

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,13 +1,26 @@
+import { number } from 'joi';
 import { getCustomRepository } from 'typeorm';
 import Customer from '../typeorm/entities/Customer';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 
-export default class ListCustomerService {
-  public async execute(): Promise<Customer[]> {
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
+class ListCustomerService {
+  public async execute(): Promise<IPaginateCustomer> {
     const customersRepository = getCustomRepository(CustomersRepository);
 
-    const customers = customersRepository.find();
+    const customers = await customersRepository.createQueryBuilder().paginate();
 
-    return customers;
+    return customers as IPaginateCustomer;
   }
 }
+
+export default ListCustomerService;
